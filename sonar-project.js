f34c6ee33fb7e21dc46113c501c8ae2986c4c6c1
arigelasaimahesh9676 <38130289+arigelasaimahesh9676@users.noreply.github.com>
2023-10-24 13:39:01 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_c38245de6343beea3b9a299702af209e39861208',
+	    'sonar.login': 'sqa_abdee6a52ce9ff23a41531670cd2b5c6099e97a3',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
